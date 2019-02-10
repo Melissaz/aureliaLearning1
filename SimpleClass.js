f529943ae0233e678d56f4352825eaa0f64afca0
melissa <melissazhu223@gmail.com>
2019-02-10 15:09:10 +1000

    first commit

---
+++ SimpleClass.js
@@ -0,0 +1,29 @@
+import {MyBase} from "MyBase";
+import {compute,val2} from "someOtherModule";
+
+export class SimpleClass extends MyBase{
+    constructor(){
+        super()
+        this.name = "Barney";
+    }
+
+    get message(){
+            let threshold = 40;
+            // let result = "Hello " + this.name + ", are you "+ (5+threshold) + " years old?";
+            let result = `Hello ${this.name}, are you ${threshold+5} years old?`;
+            return result;
+    }
+
+    calculate(){
+        return compute() + val2;
+    }
+
+    startEngine(){
+        var promise = new Promise(function(resolve, reject){
+            setTimeout(function(){
+                resolve("Roar!");
+            },5000);
+        });
+        return promise;
+    }
+}
---
+++ jspm_packages/npm/aurelia-dependency-injection@1.4.2.js
@@ -0,0 +1,3 @@
+define(["npm:aurelia-dependency-injection@1.4.2/aurelia-dependency-injection"], function(main) {
+  return main;
+});
